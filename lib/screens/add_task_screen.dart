68e8b2f7d4bff7b3753d65e6c2aafb232f994cfd
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-12-21 20:16:19 +0700

    add bottomsheet

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF757575),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            FlatButton(
+              onPressed: () {
+                //add task to the list
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.lightBlueAccent,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/add_task_screen.dart';
 import 'package:todoey_flutter/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -7,7 +8,10 @@ class TasksScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (context) => AddTaskScreen());
+        },
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
       ),
